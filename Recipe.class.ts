4c77480726f338bf0f24f6dbad086d167d8facb5
rmangat03 <33732680+rmangat03@users.noreply.github.com>
2018-05-31 16:17:10 -0700

    Add files via upload

---
+++ Recipe.class.ts
@@ -0,0 +1,31 @@
+import {Item} from "./Item.class";
+export class Recipe{
+    INGRIDENTS: Array<Item> = new Array<Item>();
+    INSTRUCTIONS: Array<string> = new Array<string>();
+    ESTIMATEDTIME: number;
+
+    /**
+     * @function addItem ADDS THE ITEMS TO THE INGRIDENT
+     * @param item ITEM FOR RECIPE TO ADD
+     */
+    addItem(item: Item){
+      if(this.INGRIDENTS.length === 0){
+        this.INGRIDENTS.push(item);
+      }else{
+        for(let i of this.INGRIDENTS)
+          if(i.name === item.name){
+            i.quantity+=item.quantity
+            return;
+          }
+          this.INGRIDENTS.push(item);
+      }
+    }
+
+    /**
+     * @function addInstruction ADD THE INSTRUCTIONS FOR THE RECIPE
+     * @param instruction INSTRUCTIONS FOR THE RECIPE
+     */
+    addInstruction(instruction: string){
+        this.INSTRUCTIONS.push(instruction);
+    }
+}

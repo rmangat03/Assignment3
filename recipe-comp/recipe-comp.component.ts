aea6a8c9079910cbd7b72ec98ce361de38fbc45e
rmangat03 <33732680+rmangat03@users.noreply.github.com>
2018-06-08 19:36:07 -0700

    refined code, added more tests

--- recipe-comp/recipe-comp.component.ts
+++ recipe-comp/recipe-comp.component.ts
@@ -7,7 +7,6 @@ import {Item} from "../Item.class"
   styleUrls: ['./recipe-comp.component.css']
 })
 export class RecipeCompComponent implements OnInit {
-  userRecipeName: string[] = [];
   userRecipeObj: Recipe[] = [];
   selectedRecipe = null
   selectedIng = null;
@@ -17,69 +16,65 @@ export class RecipeCompComponent implements OnInit {
   addOrDeleteIng: string = "";
   quantityIng: number = 0;
   userSelectedIng: string;
-  edit: number = 0;
   inst: string = "";
   letter = /^[a-zA-Z]+( [a-zA-Z]+)*$/;
 
   constructor() { }
 
   ngOnInit() {
-    this.userRecipeName.push("mango pie");
-    let recipe1 =new Recipe();
-    recipe1.addItem(new Item("mango",9));
-    recipe1.addInstruction("Peel it. shake and mix it and enjoy");
-    this.userRecipeObj.push(recipe1);
+    // let recipe1 =new Recipe();
+    // recipe1.setName("mango pie");
+    // recipe1.addItem(new Item("mango",9));
+    // recipe1.addInstruction("Peel it. shake and mix it and enjoy");
+    // this.userRecipeObj.push(recipe1);
   }
 
   addRecipe(){
-    if(!this.userRecipeName.includes(this.userRecipeNameInput) && this.addOrDeleteIng.match(this.letter) != null && this.quantityIng > 0 && this.userRecipeNameInput.match(this.letter) !== null){
+    for(let recipe of this.userRecipeObj){
+    if(recipe.NAME == this.userRecipeNameInput && this.addOrDeleteIng.match(this.letter) != null && this.quantityIng > 0 && this.userRecipeNameInput.match(this.letter) !== null){
+
       this.selectedQn = null;
       this.selectedIng = null;
-      this.selectedRecipe = null;
-      this.selectedInstruction = null;
+
+      recipe.addItem(new Item(this.addOrDeleteIng, this.quantityIng));
+
+      return;
+  }
+  }
+  if((this.addOrDeleteIng.match(this.letter) != null && this.quantityIng > 0) || this.userRecipeObj.length == 0){
+    this.selectedQn = null;
+    this.selectedIng = null;
+    this.selectedRecipe = null;
+    this.selectedInstruction = null;
 
 
-    this.userRecipeName.push(this.userRecipeNameInput);
+
     let recipeO = new Recipe();
+    recipeO.setName(this.userRecipeNameInput);
     recipeO.addItem(new Item(this.addOrDeleteIng, this.quantityIng));
     recipeO.addInstruction(this.inst);
     this.userRecipeObj.push(recipeO);
 
-  }else if(this.userRecipeName.includes(this.userRecipeNameInput) && this.addOrDeleteIng.match(this.letter) != null && this.quantityIng > 0){
-    this.selectedQn = null;
-    this.selectedIng = null;
-
-    let index = this.userRecipeName.indexOf(this.userRecipeNameInput);
-    this.userRecipeObj[index].addItem(new Item(this.addOrDeleteIng, this.quantityIng));
   }
+
   }
 
   deleteRecipe(){
     if(this.selectedRecipe !== null){
-    this.userRecipeName.splice(this.userRecipeName.indexOf(this.userRecipeNameInput), 1);
-    this.userRecipeObj.splice(this.userRecipeName.indexOf(this.userRecipeNameInput),1);
-  }
-    // for(let i of this.userRecipeObj[this.edit].INGRIDENTS){
-    //
-    //   if(this.selectedIng != null ){
-    //     let index = this.userRecipeObj[this.edit].INGRIDENTS.indexOf(this.selectIng);
-    //     this.userRecipeObj[this.edit].INGRIDENTS.[index];
-    //   }else if(i.quantity === this.selectedQn && this.quantityIng > 0){
-    //     i.quantity = this.quantityIng;
-    //
-    //   }
-    // }
-
+    for(let recipe of this.userRecipeObj){
+      if(recipe.NAME === this.selectedRecipe){
+        this.userRecipeObj.splice(this.userRecipeObj.indexOf(recipe),1);
+      }
+    }
+    }
   }
 
 
   selectRecipe(selected){
     this.selectedRecipe = selected;
-
   }
 
-  selectIng(recipe, i){
-    this.edit = i;
+  selectIng(recipe){
     this.selectedIng = recipe.name;
   }
 
@@ -93,29 +88,36 @@ export class RecipeCompComponent implements OnInit {
 
 
   editRecipe(){
-    let index = this.userRecipeName.indexOf(this.userRecipeNameInput);
-    if(index === -1 && this.userRecipeNameInput.match(this.letter) !== null){
-      this.userRecipeName[this.userRecipeName.indexOf(this.selectedRecipe)] = this.userRecipeNameInput;
+
+    for(let recipe of this.userRecipeObj){
+      if(recipe.NAME === this.selectedRecipe && this.userRecipeNameInput.match(this.letter) !== null){
+        for(let recipeCheck of this.userRecipeObj){
+          if(recipeCheck.NAME === this.userRecipeNameInput){
+            alert("Recipe already there");
+            return;
+          }
+      }
+      recipe.NAME = this.userRecipeNameInput;
+
+    }
+      if(recipe.INSTRUCTIONS[0] === this.selectedInstruction && this.inst != ""){
+        recipe.INSTRUCTIONS[0] = this.inst;
+      }
+      for(let recipeIng of recipe.INGRIDENTS){
+        if(recipeIng.name === this.selectedIng && this.addOrDeleteIng.match(this.letter) != null){
+          recipeIng.name = this.addOrDeleteIng;
+        }
+        if(recipeIng.quantity === this.selectedQn && this.quantityIng > 0){
+          recipeIng.quantity = this.quantityIng;
+        }
     }
 
-    for(let i of this.userRecipeObj[this.edit].INGRIDENTS){
-
-      if(i.name === this.selectedIng && this.addOrDeleteIng.match(this.letter) != null ){
-        i.name = this.addOrDeleteIng;
-      }else if(i.quantity === this.selectedQn && this.quantityIng > 0){
-        i.quantity = this.quantityIng;
-      }
-    }
-
-      if(this.userRecipeObj[this.edit].INSTRUCTIONS[0] === this.selectedInstruction){
-        this.userRecipeObj[this.edit].INSTRUCTIONS[0] = this.inst;
-      }
-
-
+  }
     this.selectedQn = null;
     this.selectedIng = null;
     this.selectedRecipe = null;
     this.selectedInstruction = null;
 
-  }
+
+}
 }
